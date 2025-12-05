0687f1f454fa9821683bcc8faf2e7c07657a6c28
Wan Muzaffar <wanmuz86@gmail.com>
2025-12-05 10:12:26 +0800

    code for lazyload on a function

--- src/components/ComponentA.tsx
+++ src/components/ComponentA.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
 
 const ComponentA:React.FC = () => {
+    
+    // The sum.ts function will be lazy load when the button is pressed
+    const handleClick = () => {
+        import('../utils/sum').then(({sum})=> {
+            const sumResult = sum(3,5);
+            alert(`The result is ${sumResult}`);
+        })
+
+    }
   return (
-    <div>Component A Loaded</div>
+    <div>
+        <h3>Component A Loaded</h3>
+        <p><button onClick={handleClick}>Click me</button></p>
+
+    </div>
   )
 }
 
